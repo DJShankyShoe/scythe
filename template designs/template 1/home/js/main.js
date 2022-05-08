4ce4825827bdc2b55fa67c8834defb538c035d02
Shank <62169971+DJShankyShoe@users.noreply.github.com>
2022-05-08 13:30:57 +0800

    Delete template designs directory

--- template designs/template 1/home/js/main.js
+++
@@ -1,6 +0,0 @@
-$('#getting-started').countdown('2023/01/01', function(event) { 
-	$("#day").html(event.strftime('%D <em>days</em>')); 
-	$("#hour").html(event.strftime('%H <em>hours</em>'));
-	$("#min").html(event.strftime('%M <em>minutes</em>'));
-	$("#sec").html(event.strftime('%S <em>seconds</em>'));
-	});
