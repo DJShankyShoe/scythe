768c3c46a40de4ca8edb5ebafe62ff440b774a47
Shank <62169971+DJShankyShoe@users.noreply.github.com>
2022-05-09 00:32:05 +0800

    Update process.php

--- template designs/template 1/login/process.php
+++ template designs/template 1/login/process.php
@@ -1,10 +1,10 @@
 <?php
-declare(strict_types=1);
-error_reporting(-1); // maximum errors
-ini_set('display_errors', '1');
+// declare(strict_types=1);
+// error_reporting(-1); // maximum errors
+// ini_set('display_errors', '1');
 
 session_start();
-// error_reporting(0);
+error_reporting(0);
 
 
 ?>
